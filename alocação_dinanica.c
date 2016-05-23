9b464807ad41a7dfff3285d50f416a4019fe49f7
Elivelton Costa <elivelton_costa@live.com>
2016-05-23 16:45:10 -0300

    Add files via upload

---
+++ alocação_dinanica.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{	
+	int *v, *mem; 
+	int n, i;
+	char M, E, S, I, EXIT; // M - Modificado, E - Exclusivo, S - Compartilahdo, I - Inválido, EXIT - SAIR
+	printf("Quantas cache?\n");
+	scanf ("%d", &n);
+	v = malloc (n * sizeof (int));
+	mem = malloc (n * sizeof (int));
+	while(EXIT == 0){	
+		for (i = 0; i < n; ++i){
+			scanf("%d", &v[i]);
+		}
+		for (i = 0; i < n; ++i){
+			printf("%d ", v[i]);
+		}
+		printf("\nDeseja realizar mais um comando?\n 0 - SIM                   1 - NÃO\n");
+		scanf("%d", &EXIT);
+	}
+	free (v);
+    return 0;
+}
