50b1c96794bed8ae52d549702f76ab8de5143c17
Elivelton Costa <elivelton_costa@live.com>
2016-05-24 15:16:59 -0300

    Criação do Menu da simulação do Snoop
    Verificar alocação dinâmica dentro de structs em Linguagem C

--- Main.c
+++ Main.c
@@ -1,22 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void read(){
+}
+
+void write(){
+}
+
 int main(void)
 {	
-	int *cache, *memoria; 
-	int n, i;
-	char Status_Cache; // M - Modificado, E - Exclusivo, S - Compartilahdo, I - Inv√°lido
+	/**struct Cache{
+		char status;
+		int *cache;
+	} **/
+	
+	int *memoria, *cache; 
+	int n, i, menu;
+	char Status_Cache; // M - Modificado, E - Exclusivo, S - Compartilhado, I - Inv·lido
+	printf("#############################################\n");
+	printf("#           Cache Memory Snooping           #\n");
+	printf("#############################################\n");
 	printf("Quantas cache?\n");
 	scanf ("%d", &n);
 	cache = malloc (n * sizeof (int));
 	memoria = malloc (n * sizeof (int));
-	for (i = 0; i < n; ++i){
-		scanf("%d", &cache[i]);
-	}
-	for (i = 0; i < n; ++i){
-		printf("%d ", &cache[i]);
+	while(menu != 0){
+		printf("Quais opÁıes vocÍ deseja?\n");
+		printf("1 - LEITURA\n2 - ESCRITA\n3 - LISTAR ENDERE«OS DA CACHE\n4 - LISTAR ENDERE«OS DA MEM”RIA\n0 - SAIR\n");
+		scanf("%d", &menu);
+		if(menu == 1){
+			for (i = 0; i < n; ++i){
+			printf("%d\n", cache[i]);
+		}
+		}
+		if(menu == 2){
+			for (i = 0; i < n; ++i){
+			scanf("%d", cache[i]);
+		}
+		}
+		if(menu == 3){
+			for (i = 0; i < n; ++i){
+			printf("Cache %d: %d\n", i, &cache[i]);
+			}
+		}
+		if(menu == 4){
+			for (i = 0; i < n; ++i){
+			printf("MemÛria %d: %d\n", i, &memoria[i]);
+			}
+		}
 	}
 	
-	free (v);
+	free (cache);
     return 0;
 }
